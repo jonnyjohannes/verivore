2a6dd064597361a98c80ff5b3644826bcdf126df
Jonny <ComScan@comscan.bootuplabs.sutuscentral.com>
2011-02-01 16:24:39 -0800

    Tagging capabilities for posts implemented, still need to show the tags in a useful way.
    Google Analytics added to index for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,8 +13,12 @@
 
 class Post < ActiveRecord::Base
   validates_presence_of :body, :title 
-	has_many :comments, :order => "created_at DESC"
+	has_many :comments, :order => "created_at DESC", :dependent => :destroy
+	has_many :taggings, :dependent => :destroy
+	has_many :tags, :through => :taggings
 	has_permalink :title, :update => true
+	after_save :assign_tags
+	attr_writer :tag_names
 	
   cattr_reader :per_page
   @@per_page = 5
@@ -27,6 +31,19 @@ class Post < ActiveRecord::Base
     wordcount = 50
     body.split[0..(wordcount-1)].join(" ") + (body.split.size > wordcount ? " ... " : "")
   end
-    
+  
+  def tag_names
+    @tag_names || tags.map(&:name).join(' ')
+  end
+  
+  private
+  def assign_tags
+    if @tag_names
+      self.tags = @tag_names.split(/\s+/).map do |name|
+        Tag.find_or_create_by_name(name)
+      end
+    end
+  end
+  
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110112002222) do
+ActiveRecord::Schema.define(:version => 20110201232600) do
 
   create_table "canned_foods", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,19 @@ ActiveRecord::Schema.define(:version => 20110112002222) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "permalink"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
