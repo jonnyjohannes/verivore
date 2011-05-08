0e91db221c87b3e61f73499e9325d40bf0d9fe3b
Jonny <ComScan@ComScan.local>
2011-05-08 00:49:24 -0700

    order the tags for display

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,14 @@
 class TagsController < ApplicationController
 
-def index
-  @tags = Tag.paginate :page => params[:page], :order => 'created_at DESC'
-end
+  def index
+    @unsorted_tags = Tag.all
+    @tag_alph_sort = @unsorted_tags.sort { |a,b| a.name.downcase <=> b.name.downcase }
+    @tags = @tag_alph_sort.paginate :page => params[:page]
+  end
 
-def show
-  @tag = Tag.find(params[:id])
-  @tagged_posts = @tag.posts.paginate :page => params[:page], :order => 'created_at DESC'
-end
+  def show
+    @tag = Tag.find(params[:id])
+    @tagged_posts = @tag.posts.paginate :page => params[:page], :order => 'created_at DESC'
+  end
 
 end
