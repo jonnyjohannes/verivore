d592accd646d04994b5e95335603ac1b4ed23f9d
Jonny Johannes <jonny@jonnyjohannes.com>
2016-02-03 21:43:54 -0700

    friendly_id update #friendly.find

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.xml
   def show
-    @post = Post.find(params[:id])
+    @post = Post.friendly.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
